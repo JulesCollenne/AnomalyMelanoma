c91c2c35d6cae8c61ac9edea4635c62dbd83992b
JulesCollenne <jules.collenne@univ-amu.fr>
2023-09-25 17:07:10 +0200

    update rl training

--- training_setups/training_setup.py
+++ training_setups/training_setup.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.utils.data import DataLoader
 
 from dataset import CustomDataset
 
@@ -11,9 +12,30 @@ class TrainingSetup(nn.Module):
         self.dataset = CustomDataset("data.csv", transform=self.base_model.transform)
 
     def kfold_training(self, kf):
-        for fold, (train_idx, val_idx) in enumerate(kf.split(dataset)):
-            train_data = torch.utils.data.Subset(dataset, train_idx)
-            val_data = torch.utils.data.Subset(dataset, val_idx)
+        for fold, (train_idx, val_idx) in enumerate(kf.split(self.dataset)):
+            train_data = torch.utils.data.Subset(self.dataset, train_idx)
+            val_data = torch.utils.data.Subset(self.dataset, val_idx)
 
-            train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-            val_loader = DataLoader(val_data, batch_size=batch_size)
+            train_loader = DataLoader(train_data, batch_size=self.base_model.batch_size, shuffle=True)
+            val_loader = DataLoader(val_data, batch_size=self.base_model.batch_size)
+
+            optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-6)
+            criterion = nn.CosineEmbeddingLoss()
+
+            for epoch in range(num_epochs):
+                model.train()
+                # Training loop here
+                for batch in train_loader:
+                    # Implement your training logic
+                    pass
+
+                # Validation loop here
+                model.eval()
+                with torch.no_grad():
+                    # Implement your validation logic
+                    pass
+
+            # Save the trained model
+            model_path = f"fold-{fold}.pth"
+            torch.save(model.state_dict(), model_path)
+            print(f"Saved model for fold {fold} as {model_path}")
